e1914603fa9e121ad8fc529727cb74fdd339def9
Desbouis Lenny <lennydesbouis.pro@gmail.com>
2026-08-26 12:29:57 +0200

    fixing small console errors

--- srcs/frontend/app/layout.tsx
+++ srcs/frontend/app/layout.tsx
@@ -9,11 +9,13 @@ import { GameProvider } from './context/GameContext';
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
+	preload: false,
 });
 
 const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
 	subsets: ["latin"],
+	preload: false,
 });
 
 export const metadata: Metadata = {
